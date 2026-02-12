56a3b1b8f15c79d94ca5c5e83fdd7a4447f8bed5
anantharjun672001-a11y <anantharjun672001@gmail.com>
2026-02-12 15:11:16 +0530

    controllers,router folder created and data getted successfully

--- index.js
+++ index.js
@@ -2,20 +2,39 @@ import http from "http";
 import dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
+import productRoute from "./Routers/productRoute.js"
+
+//dotenv config
 
 dotenv.config();
 
-const app = express();   
+//express initialization
 
-const port = process.env.PORT || 5000;
+const app = express();  
+
+
+
+//default middleware
 
 app.use(express.json());
-app.use(cors())
+
+app.use(cors());
+
+//default route
 
 app.get("/", (req, res) => {
     res.status(200).send(`<h1 style="text-align:center;">Welcome to Backend</h1>`);
 });
 
+//custom routes
+app.use("/api/products",productRoute);
+
+//port
+
+const port = process.env.PORT || 5000;
+
 app.listen(port, () => {
     console.log(`Server started on port ${port}`);
 });
+
+
